a063203c672aaa36a212986c61193223592361c2
Musadique Muslim <musadique1997@gmail.com>
2023-10-21 22:26:39 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,233 @@
+// Chapter 6-9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Chapter 9-11
+
+ // Question : 1
+
+// city = prompt("Enter your city name")
+// if(city == "karachi"){
+//     alert("Welcome to city of lights")
+// }
+// else{
+//     alert("Welcome!")
+// }
+
+  // Question : 2
+
+//   user = prompt("Type Your Gender")
+// if(user == "male"){
+//     alert("Good Morning! Sir")
+// }
+// else if(user == "female")
+// {
+//     alert("Good Morning Ma,am")
+// }
+// else{
+//     alert("Again Type!")
+// }
+
+
+   // Question : 3
+
+//    color = prompt("Type Signal Color")
+//    if(color == "red")
+//    {
+//     alert("Must stop")
+//    }
+//    else if(color == "yellow")
+//    {
+//     alert("Ready to move")
+//    }
+//    else if(color == "green")
+//    {
+//     alert("Move now")
+//    }
+//    else{
+//     alert("again type")
+//    }
+
+   // Question : 4
+//    fuel = prompt("Type Current fuel in your car in litres")
+//    if(fuel < "0.25litres" ){
+//     alert("Please refill the fuel in your car")
+//    }
+//    else{
+//     alert("Ok!")
+//    }
+
+    // Question 5
+
+   //   var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+
+// if (c === 13){
+//    alert("condition 2 is true");
+//    }
+
+// if (++c < 14){
+//    alert("condition 3 is true");
+//    }
+
+// if(c === 14){
+//    alert("condition 4 is true");
+//    }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals" + " " + totalCost);
+// }
+
+// if (true){
+//    alert("True");
+//    }
+
+// if (false){
+//    alert("False");
+//    }
+
+// if("car" < "cat"){
+//    alert("car is smaller than cat");
+//    }
+
+// var eng, urdu, math, mo, per, grade, remark
+// eng = +prompt("Enter your English paper marks out of 100")
+// urdu = +prompt("Enter your Urdu paper marks out of 100")
+// math = +prompt("Enter your Maths paper marks out of 100")
+
+// document.write("Total Marks : 300 <br>" )
+
+// mo = eng + urdu + math
+// document.write("Obtained Marks :" + " " + mo + "<br>")
+// per = (mo/300)*100
+// document.write("Percentage :" + " " + per + "%" + "<br>")
+
+// if(per >= "80"){
+//    document.write("Grade : A-One" + "<br>")
+// }
+// else if(per >= "70"){
+//    document.write("Grade : A" + "<br>")
+// }
+// else if(per >= "60"){
+//    document.write("Grade : B" + "<br>")
+// }
+// else {
+//    document.write("Grade : Fail" + "<br>")
+// }
+//  if(per >= "80"){
+//    document.write("Remarks : Excellent")
+//  }
+//  else  if(per >= "70"){
+//    document.write("Remarks : Good")
+//  }
+//  else  if(per >= "60"){
+//    document.write("Remarks : You need to improve")
+//  }
+//  else {
+//    document.write("Remarks : Sorry")
+//  }
+
+
+   //Question : 8
+
+   // a = +prompt("Enter any number and check divisible by 3 or not")
+   // if(a%3 == "0")
+   // {
+   //    alert("This number is divisble by 3")
+   // }
+   // else{
+   //    alert("This number is not divisble by 3")
+   // }
+
+
+   // Question : 9
+   // a = +prompt("Enter any number and check even or add")
+   // if(a%2 == "0")
+   // {
+   //    alert("This number is even")
+   // }
+   // else{
+   //    alert("This number is odd")
+   // }
+
+
+   // Question : 10
+
+   // T = +prompt("Please enter the value of Temperature")
+
+   // if(T > "40"){
+   //    alert("It is too hot outside")
+   // }
+   // else  if(T > "30"){
+   //    alert("The weather today is normal")
+   // }
+   // else  if(T > "20"){
+   //    alert("Today's weather is cool")
+   // }
+   // else  if(T <= "20"){
+   //    alert("OMG! today weather is so cool")
+   // }
+
+
+   // Question : 11
+
+   // var a
+   // var operator
+   // var c
+   // var result
+   // a = +prompt("Enter First Value")
+   
+   // operator = prompt("Used any one operator (+ , - , * , / , % )")
+   
+   // c = +prompt("Enter Second Value")
+   
+   
+   // if(operator == "+"){
+   //     alert(result = a+c)
+   // }
+   // else if(operator == "-"){
+   // alert(a-c)
+       
+   // }
+   // else if(operator == "*"){
+   // alert(a*c)
+       
+   // }
+   // else if(operator == "/"){
+   // alert(a/c)
+       
+   // }
+   // else if(operator == "%"){
+   // alert(a%c)
+       
+   // }
+   // else{
+   //     alert("Try again")
+   // }
+   
